2ab6d2669f5f9c853855d65ec0060dd806497a0f
WYC <43289907+me356500@users.noreply.github.com>
2021-12-23 21:18:44 +0800

    Create headers.h

---
+++ headers.h
@@ -0,0 +1,25 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+#include <bits/stdc++.h>
+#include <ctype.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <strings.h>
+#include <sys/uio.h>
+#include <sys/time.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+
+using namespace std;
+//cd /home/w/Desktop/OS/np_hw2/final
+#define RECV_TIMEO_USEC 100000
+#define SEND_TIMEO_USEC 250000
